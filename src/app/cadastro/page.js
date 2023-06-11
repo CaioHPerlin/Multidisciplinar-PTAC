460e15c3439641e87b1c5e7cb549e03130fb752c
Caio H Perlin <caiohperlin@gmail.com>
2023-06-11 19:50:15 -0400

    Formulário de Cadastro Adicionado

---
+++ src/app/cadastro/page.js
@@ -0,0 +1,51 @@
+'use client';
+import { useState } from 'react';
+import '../globals.css';
+import Link from 'next/link';
+//Imagem que carrega conforme o usuário digita o link (+)
+//Verificar formato da DATACADASTRO
+
+export default function Cadastro() {
+  const [titulo, setTitulo] = useState('');
+  const [preco, setPreco] = useState();
+  const [descricao, setDescricao] = useState('');
+  const [imagem, setImagem] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const dataCadastro = new Date(Date.now()).toLocaleDateString();
+    const produto = {
+      titulo: titulo,
+      dataCadastro: dataCadastro,
+      preco: preco,
+      descricao: descricao,
+      imagem: imagem,
+    };
+
+    console.log(produto);
+    setTitulo("");
+    setPreco("");
+    setDescricao("");
+    setImagem("");
+  };
+
+  return (
+    <main className="select-none bg-neutral-950 grid grid-cols-1 justify-items-center">
+      <div className="m-20 grid grid-cols-2 gap-4">
+        <Link href="/" className="bg-red-800 p-2">
+          HOME
+        </Link>
+        <Link href="/cadastro" className="bg-red-800 p-2">
+          CADASTRO
+        </Link>
+      </div>
+      <form onSubmit={handleSubmit} className="grid grid-cols-1 w-2/4 gap-4 p-6 m-12 bg-red-700 border-red-800 border-r-8">
+        <input required value={titulo} className="w-50 p-1" type="text" placeholder="Nome do Produto" onChange={e => setTitulo(e.target.value)}/>
+        <input required value={preco}className="w-50 p-1" type="number" placeholder="Insira Preço do Produto" onChange={e => setPreco(e.target.value)}/>
+        <input required value={descricao} className="w-50 p-1" type="text" placeholder="Descrição do Produto" onChange={e => setDescricao(e.target.value)}/>
+        <input required value={imagem} className="w-50 p-1" type="text" placeholder="URL da Imagem" onChange={e => setImagem(e.target.value)}/>
+        <button type="submit">Enviar</button>
+      </form>
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,23 @@
-"use client"
-import "./globals.css"
-import Image from "next/image";
+'use client';
+import Link from 'next/link';
+import './globals.css';
+import Image from 'next/image';
 
 export default async function Home() {
-
-  const req = await fetch("http://localhost:3001/produtos", {
-    next: {revalidate: 1}
+  const req = await fetch('http://localhost:3001/produtos', {
+    next: { revalidate: 1 },
   });
   const produtos = await req.json();
 
   return (
     <main className="select-none bg-neutral-950 flex flex-wrap">
-      <div className="m-20"></div>
+      <div className="m-20">
+        <Link href="/cadastro" className="bg-red-800 p-2">
+          CADASTRO
+        </Link>
+      </div>
       <div className="p-6 m-12 mt-36 bg-red-700 border-red-800 border-r-8">
-        {produtos.map(produto => (
+        {produtos.map((produto) => (
           <div key={produto.id}>
             <h1>{produto.titulo}</h1>
             <h2>{produto.preco}</h2>
@@ -29,5 +33,5 @@ export default async function Home() {
         ))}
       </div>
     </main>
-  )
+  );
 }
